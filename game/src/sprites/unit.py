4b6f6be7ddc480521d1268c82063f537cda0153b
Kaapo Kivioja <kaapo.kivioja@helsinki.fi>
2022-04-16 23:32:43 +0300

    Update sprites

--- game/src/sprites/unit.py
+++ game/src/sprites/unit.py
@@ -6,10 +6,11 @@ dirname = os.path.dirname(__file__)
 class Unit(pygame.sprite.Sprite):
     def __init__(self, x, y, unit_type):
         super().__init__()
-        
         self.type = unit_type
 
-        self.image = pygame.image.load(os.path.join(dirname, "..", "assets", f"Unit{self.type}.png"))
+        self.image = pygame.image.load(
+            os.path.join(dirname, "..", "assets", f"Unit{self.type}.png")
+            )
 
         self.rect = self.image.get_rect()
 
