4ca5297e4f17baf1c9cc1d119561b4b8d3eefc90
Kaapo Kivioja <kaapo.kivioja@helsinki.fi>
2022-05-15 19:52:41 +0300

    Changed variable names

--- game/src/renderer.py
+++ game/src/renderer.py
@@ -2,11 +2,11 @@ import pygame
 
 
 class Renderer:
-    def __init__(self, display, level):
+    def __init__(self, display, board):
         self._display = display
-        self._level = level
+        self._board = board
 
     def render(self):
-        self._level.all_sprites.draw(self._display)
+        self._board.all_sprites.draw(self._display)
 
         pygame.display.update()
